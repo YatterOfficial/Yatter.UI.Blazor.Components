2b4b4ffc328cebe9d6cc18dfdbd6637df1d5eb0f
Count Anthony Harrison <anthony@harrisonofthenorth.com>
2021-09-24 08:11:58 +0100

    Initial Commit from namespace transfer HarryHotdog => Yatter

---
+++ README.md
@@ -0,0 +1,21 @@
+## Alpha Alert!
+
+We are progressively building this solution from a custom ListView pattern that we already use in Xamarin Forms, and as you can't read our mind, you wont know from one week to another where we are going on this. So beware. We are, however, developing on various branches, and commiting approved updates to the master branch, so the master branch can be considered to have better form than the development branches. It's a winding road to Camelot!
+
+# HarryHotdog.UI.Blazor.Components
+
+This library has been created to render a collection of lightweight List ```Description``` objects from JSON or XML list descriptions, which when used in conjunction with the ListBuilder that generates or consumes such JSON or XML, will allow rapid list creation without needing to know much about the underlying HTML or CSS technologies that Blazor builds upon.
+
+To date, this Blazor renderer allows lightweight lists to be generated on the following devices:
+
+- Website, using Blazor, on any server or device where Blazor can be deployed.
+- iOS, using .Net Maui Blazor
+- Android, using .Net Maui Blazor
+- MacOS, using .Net Maui Blazor
+- Windows, using .Net Maui Blazor
+
+The ListBuilder that this Blazor renderer has been created upon is at [HarryHotdog.UI.ListBuilder](https://github.com/HarrisonOfTheNorth/HarryHotdog.UI.ListBuilder)
+
+The API that integrates both of these Libraries, is at [Yatter API](https://github.com/HarrisonOfTheNorth/Yatter)
+
+This demo also uses our ResponsiveHttpClient at [https://github.com/HarrisonOfTheNorth/HarryHotdog.Http.ResponsiveHttpClient](https://github.com/HarrisonOfTheNorth/HarryHotdog.Http.ResponsiveHttpClient)
---
+++ Yatter.UI.Blazor.Components/Framework/ComponentMetadata.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using Data = Yatter.UI.ListBuilder.Framework;
+using Component = Yatter.UI.Blazor.Components.ListItems;
+
+namespace Yatter.UI.Blazor.Components.Framework
+{
+    public static class ComponentMetadataInfo
+    {
+
+        public static Dictionary<string, Data.ComponentMetadata> ComponentMetadataDictionary => _componentMetadata;
+
+        private static readonly Dictionary<string, Data.ComponentMetadata> _componentMetadata =
+            new()
+            {
+                {
+                    "H1",
+                    new Data.ComponentMetadata
+                    {
+                        Name = "H1",
+                        Type = typeof(Component.H1)
+                    }
+                },
+                {
+                    "H2",
+                    new Data.ComponentMetadata
+                    {
+                        Name = "H2",
+                        Type = typeof(Component.H2)
+                    }
+                },
+                {
+                    "H3",
+                    new Data.ComponentMetadata
+                    {
+                        Name = "H3",
+                        Type = typeof(Component.H3)
+                    }
+                },
+                {
+                    "H4",
+                    new Data.ComponentMetadata
+                    {
+                        Name = "H4",
+                        Type = typeof(Component.H4)
+                    }
+                },
+                {
+                    "H5",
+                    new Data.ComponentMetadata
+                    {
+                        Name = "H5",
+                        Type = typeof(Component.H5)
+                    }
+                },
+                {
+                    "Label",
+                    new Data.ComponentMetadata
+                    {
+                        Name = "Label",
+                        Type = typeof(Component.Label)
+                    }
+                },
+                {
+                    "Base64Image",
+                    new Data.ComponentMetadata
+                    {
+                        Name = "Base64Image",
+                        Type = typeof(Component.Base64Image)
+                    }
+                },
+                {
+                    "Entry",
+                    new Data.ComponentMetadata
+                    {
+                        Name = "Entry",
+                        Type = typeof(Component.Entry)
+                    }
+                }
+            };
+
+    }
+}
+
